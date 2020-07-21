1746e0e51b7d6ca1f9cbe026f460002ff174d8d9
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2020-07-20 21:36:29 -0400

    call back function

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// "/" is the root and the function is a callback function
+app.get("/", function(request, respond){
+    console.log(request);
+
+});
+
 app.listen(3000, function(){
     console.log("Server Started on port 3000");
 });
