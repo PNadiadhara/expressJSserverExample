3ffae1e425ac83c2304f12874cb6574f212bf52e
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2020-07-27 20:53:13 -0400

    routes

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require("express");
 
 const app = express();
 
-// "/" is the root and the function is a callback function
+// "/" is the route and the function is a callback function
 // function(request, respond) often shortend to function(req, res)
+// imagine "/" as home page
 app.get("/", function(request, respond){
     //console.log(request);
     // respond can send plain text or html
     // server info
     respond.send("<h1> Hello World </h1>");
 
+});
+
+// imagine "/contact" as contact page
+app.get("/contact", function(req, res){
+     res.send("contact me at email@email.com")
+});
+
+app.get("/about", function(req, res){
+    res.send("My name is!")
 });
 
 app.listen(3000, function(){
